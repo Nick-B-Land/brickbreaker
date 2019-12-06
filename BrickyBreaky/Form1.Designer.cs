a8257a8f06443d92c118695992f2197daa538338
Gurpreet Sehmbey <g.sehmbey985@mybvc.ca>
2019-12-05 17:30:20 -0700

    Collision and ball moving

---
+++ BrickyBreaky/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace BrickyBreaky
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.board = new System.Windows.Forms.Panel();
+            this.paddle = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.board.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.paddle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // board
+            // 
+            this.board.Controls.Add(this.ball);
+            this.board.Controls.Add(this.paddle);
+            this.board.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.board.Location = new System.Drawing.Point(0, 0);
+            this.board.Name = "board";
+            this.board.Size = new System.Drawing.Size(800, 450);
+            this.board.TabIndex = 0;
+            this.board.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Board_PreviewKeyDown);
+            // 
+            // paddle
+            // 
+            this.paddle.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.paddle.Location = new System.Drawing.Point(257, 385);
+            this.paddle.Name = "paddle";
+            this.paddle.Size = new System.Drawing.Size(140, 20);
+            this.paddle.TabIndex = 0;
+            this.paddle.TabStop = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(233, 168);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.TabIndex = 1;
+            this.ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.board);
+            this.Name = "Form1";
+            this.Text = "Bricky Breaky";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.board.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.paddle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel board;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox paddle;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ BrickyBreaky/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BrickyBreaky
+{
+    public partial class Form1 : Form
+    {
+        // speed for ball
+        public int speedX = 4;
+        public int speedY = 4;
+        // points counter
+        public int points = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            Cursor.Hide();
+
+            this.FormBorderStyle = FormBorderStyle.None; //removes border
+            this.TopMost = true; // brings the form to the front
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            paddle.Top = board.Bottom - (board.Bottom / 10); // setting the position of the paddle
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Board_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if(e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            paddle.Left = Cursor.Position.X - (paddle.Width / 2);
+
+            ball.Left += speedX;
+            ball.Top += speedY;
+
+            if(ball.Bottom >= paddle.Top && ball.Bottom <= paddle.Bottom && ball.Left >= paddle.Left && ball.Right <= paddle.Right)
+            {
+                speedY += 2;
+                speedX += 2;
+
+                speedY = -speedY;
+                points += 1;
+            }
+            if (ball.Left <= board.Left)
+            {
+                speedX = -speedX;
+            }
+            if (ball.Right >= board.Right)
+            {
+                speedX = -speedX;
+            }
+            if (ball.Top <= board.Top)
+            {
+                speedY = -speedY;
+            }
+            if (ball.Bottom >= board.Bottom)
+            {
+                timer1.Enabled = false; // stop the game 
+            }
+        }
+    }
+}

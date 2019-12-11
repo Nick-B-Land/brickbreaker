0a90dc80434936d04e3c10393b61a4d0a665a6b2
ScottPatt <s.patterson528@mybvc.ca>
2019-12-11 13:44:29 -0700

    Commit me son

--- BrickyBreaky/Form1.Designer.cs
+++ BrickyBreaky/Form1.Designer.cs
@@ -52,8 +52,9 @@
             this.board.Controls.Add(this.scoreNameLbl);
             this.board.Dock = System.Windows.Forms.DockStyle.Fill;
             this.board.Location = new System.Drawing.Point(0, 0);
+            this.board.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.board.Name = "board";
-            this.board.Size = new System.Drawing.Size(800, 504);
+            this.board.Size = new System.Drawing.Size(1067, 620);
             this.board.TabIndex = 0;
             this.board.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Board_PreviewKeyDown);
             // 
@@ -61,9 +62,10 @@
             // 
             this.scoreLbl.AutoSize = true;
             this.scoreLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scoreLbl.Location = new System.Drawing.Point(737, 9);
+            this.scoreLbl.Location = new System.Drawing.Point(983, 11);
+            this.scoreLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.scoreLbl.Name = "scoreLbl";
-            this.scoreLbl.Size = new System.Drawing.Size(60, 24);
+            this.scoreLbl.Size = new System.Drawing.Size(79, 29);
             this.scoreLbl.TabIndex = 5;
             this.scoreLbl.Text = "label1";
             // 
@@ -71,18 +73,20 @@
             // 
             this.lvlNameLbl.AutoSize = true;
             this.lvlNameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lvlNameLbl.Location = new System.Drawing.Point(3, 9);
+            this.lvlNameLbl.Location = new System.Drawing.Point(4, 11);
+            this.lvlNameLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lvlNameLbl.Name = "lvlNameLbl";
-            this.lvlNameLbl.Size = new System.Drawing.Size(65, 24);
+            this.lvlNameLbl.Size = new System.Drawing.Size(83, 29);
             this.lvlNameLbl.TabIndex = 2;
             this.lvlNameLbl.Text = "Level: ";
             // 
             // ball
             // 
             this.ball.BackColor = System.Drawing.Color.Red;
-            this.ball.Location = new System.Drawing.Point(222, 282);
+            this.ball.Location = new System.Drawing.Point(296, 347);
+            this.ball.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ball.Name = "ball";
-            this.ball.Size = new System.Drawing.Size(20, 20);
+            this.ball.Size = new System.Drawing.Size(27, 25);
             this.ball.TabIndex = 1;
             this.ball.TabStop = false;
             // 
@@ -90,18 +94,20 @@
             // 
             this.lvlLbl.AutoSize = true;
             this.lvlLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lvlLbl.Location = new System.Drawing.Point(74, 9);
+            this.lvlLbl.Location = new System.Drawing.Point(99, 11);
+            this.lvlLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lvlLbl.Name = "lvlLbl";
-            this.lvlLbl.Size = new System.Drawing.Size(60, 24);
+            this.lvlLbl.Size = new System.Drawing.Size(79, 29);
             this.lvlLbl.TabIndex = 4;
             this.lvlLbl.Text = "label1";
             // 
             // paddle
             // 
             this.paddle.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.paddle.Location = new System.Drawing.Point(303, 472);
+            this.paddle.Location = new System.Drawing.Point(404, 581);
+            this.paddle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.paddle.Name = "paddle";
-            this.paddle.Size = new System.Drawing.Size(140, 20);
+            this.paddle.Size = new System.Drawing.Size(187, 25);
             this.paddle.TabIndex = 0;
             this.paddle.TabStop = false;
             // 
@@ -109,24 +115,28 @@
             // 
             this.scoreNameLbl.AutoSize = true;
             this.scoreNameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scoreNameLbl.Location = new System.Drawing.Point(661, 9);
+            this.scoreNameLbl.Location = new System.Drawing.Point(881, 11);
+            this.scoreNameLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.scoreNameLbl.Name = "scoreNameLbl";
-            this.scoreNameLbl.Size = new System.Drawing.Size(70, 24);
+            this.scoreNameLbl.Size = new System.Drawing.Size(89, 29);
             this.scoreNameLbl.TabIndex = 3;
             this.scoreNameLbl.Text = "Score: ";
             this.scoreNameLbl.Click += new System.EventHandler(this.scoreNameLbl_Click);
             // 
             // timer1
             // 
+            this.timer1.Interval = 90;
             this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 504);
+            this.ClientSize = new System.Drawing.Size(1067, 620);
             this.Controls.Add(this.board);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Bricky Breaky";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
